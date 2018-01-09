1b7d9bd1c761b2d07eb17c21dcc1c0f05f92993b
Daniel Huang <gn03249822@DanieltekiMacBook-Pro-2.local>
2018-01-09 03:10:47 -0800

    hover feature implemented

--- plotting.py
+++ plotting.py
@@ -1,13 +1,22 @@
 from motion_detector import df
 
 from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
 
+#convert the datetime datatype to string
+df["Start_str"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_str"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+cds = ColumnDataSource(df)
 p = figure(x_axis_type = "datetime", height = 100, width = 500, responsive = True, title = "Motion Graph")
 p.yaxis.minor_tick_line_color = None #hide the unnecessary ticks on y-axis
 
 p.ygrid[0].ticker.desired_num_ticks = 1 #hide the intermmediate lines/grid in the graph
 #draw the bar chart with the quad glyph
-q = p.quad(left = df['Start'], right = df['End'], top=1, bottom=0, color = "green")
+
+hover = HoverTool(tooltips = [("Start", "@Start_str"), ("End", "@End_str")])
+p.add_tools(hover)
+q = p.quad(left = "Start", right = "End", top=1, bottom=0, color = "green",source = cds)
 
 output_file("Graph.html")
 show(p)
