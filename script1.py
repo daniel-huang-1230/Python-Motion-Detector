59a88da12cf3868426535a176e65f09d8322f4c7
Daniel Huang <gn03249822@DanieltekiMacBook-Pro-2.local>
2018-01-08 16:44:36 -0800

    time generated Done

--- script1.py
+++ script1.py
@@ -1,10 +1,17 @@
-import cv2, time
+import cv2, time, pandas
+from datetime import datetime
 
 first_frame = None
 video = cv2.VideoCapture(0)  #0 is the default camera (webcam) of my computer
+status_list = [None, None]  #to prevent index out of bound error at the beginning
+times = []
+df = pandas.DataFrame(columns= ["Start", "End"])
+
+
 time.sleep(2)
 while True:
     check, frame = video.read()
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     gray = cv2.GaussianBlur(gray, (21, 21), 0)  # for better accuracy
@@ -14,7 +21,7 @@ while True:
         continue  # skip the rest of the lines in this iteration
 
     delta_frame = cv2.absdiff(first_frame,gray)
-    thres_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thres_frame = cv2.threshold(delta_frame, 80, 255, cv2.THRESH_BINARY)[1]
     thres_frame = cv2.dilate(thres_frame, None, iterations=2)
 
     # find all contours in the thres_frame
@@ -25,9 +32,17 @@ while True:
         if cv2.contourArea(contour) < 10000:
             continue
         else:
+            status = 1   #first object found entering the frame
             # draw a rectangle around the contour
             (x, y, w, h) = cv2.boundingRect(contour)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)  # green rectangle
+
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
+
 
 
     cv2.imshow("Gray Frame", gray)
@@ -38,7 +53,17 @@ while True:
     key = cv2.waitKey(1)
 
     if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())  #treat quitting as the last exit time
         break
+    #end of loop
 
+print(status_list)
+print(times)
+
+for i in range(0, len(times),2):
+    df = df.append({"Start":times[i], "End":times[i+1]}, ignore_index= True)
+
+df.to_csv("Times.csv") #output the csv file for plotting
 video.release()
 cv2.destroyAllWindows()
