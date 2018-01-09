7018b1ded7d2369b7464d5e02724107e7966f173
Daniel Huang <gn03249822@DanieltekiMacBook-Pro-2.local>
2018-01-09 02:18:30 -0800

    plot styling improved

--- motion_detector.py
+++ motion_detector.py
@@ -2,18 +2,17 @@ import cv2, time, pandas
 from datetime import datetime
 
 first_frame = None
-video = cv2.VideoCapture(0)  #0 is the default camera (webcam) of my computer
 status_list = [None, None]  #to prevent index out of bound error at the beginning
 times = []
 df = pandas.DataFrame(columns= ["Start", "End"])
 
+video = cv2.VideoCapture(0)  #0 is the default camera (webcam) of my computer
 
 time.sleep(2)
 while True:
     check, frame = video.read()
     status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     gray = cv2.GaussianBlur(gray, (21, 21), 0)  # for better accuracy
 
     if first_frame is None:
@@ -31,11 +30,11 @@ while True:
     for contour in cnts:
         if cv2.contourArea(contour) < 10000:
             continue
-        else:
-            status = 1   #first object found entering the frame
-            # draw a rectangle around the contour
-            (x, y, w, h) = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)  # green rectangle
+
+        status = 1   #first object found entering the frame
+        # draw a rectangle around the contour
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)  # green rectangle
 
     status_list.append(status)
 
@@ -64,7 +63,7 @@ print(status_list)
 print(times)
 
 for i in range(0, len(times),2):
-    df = df.append({"Start":times[i], "End":times[i+1]}, ignore_index= True)
+    df = df.append({"Start":times[i], "End":times[i+1]}, ignore_index = True)
 
 df.to_csv("Times.csv") #output the csv file for plotting
 
--- plotting.py
+++ plotting.py
@@ -3,8 +3,10 @@ from motion_detector import df
 from bokeh.plotting import figure, show, output_file
 
 p = figure(x_axis_type = "datetime", height = 100, width = 500, responsive = True, title = "Motion Graph")
+p.yaxis.minor_tick_line_color = None #hide the unnecessary ticks on y-axis
 
-#
+p.ygrid[0].ticker.desired_num_ticks = 1 #hide the intermmediate lines/grid in the graph
+#draw the bar chart with the quad glyph
 q = p.quad(left = df['Start'], right = df['End'], top=1, bottom=0, color = "green")
 
 output_file("Graph.html")
